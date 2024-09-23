b3ff78b6f1fd852669f1763508a9d0380385c450
GustavoSept <GustavoSept.Cron@gmail.com>
2024-09-23 09:26:09 -0300

    hotfix: PrismaService imported, port configured

--- ez_decks/src/main.ts
+++ ez_decks/src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+   const appPort: string = process.env.NODEAPP_PORT || '4000';
+
+   // Now appPort is either a number or undefined
+   console.log(`App port is: ${appPort}`);
+
    const app = await NestFactory.create(AppModule);
-   await app.listen(3000);
+   await app.listen(appPort);
 }
 bootstrap();
