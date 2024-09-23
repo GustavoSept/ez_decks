1a6330ea06860a55e6eb1e995a4ef05b7dce1e4e
GustavoSept <GustavoSept.Cron@gmail.com>
2024-09-23 14:13:39 -0300

    implements global pipe, and api test backup

--- ez_decks/src/main.ts
+++ ez_decks/src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
    const appPort: string = process.env.NODEAPP_PORT || '4000';
 
-   // Now appPort is either a number or undefined
-   console.log(`App port is: ${appPort}`);
-
    const app = await NestFactory.create(AppModule);
+
+   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
    await app.listen(appPort);
 }
 bootstrap();
