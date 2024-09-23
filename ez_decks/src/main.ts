a49d237b419ae2ea6780fe871fea99a17897134a
GustavoSept <GustavoSept.Cron@gmail.com>
2024-09-23 15:32:47 -0300

    implements stricter pipe

--- ez_decks/src/main.ts
+++ ez_decks/src/main.ts
@@ -7,7 +7,13 @@ async function bootstrap() {
 
    const app = await NestFactory.create(AppModule);
 
-   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+   app.useGlobalPipes(
+      new ValidationPipe({
+         whitelist: true,
+         forbidNonWhitelisted: true,
+         transform: true,
+      })
+   );
 
    await app.listen(appPort);
 }
