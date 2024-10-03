715e7bceb287936f7c93a35ec6298eff5f8cee31
GustavoSept <GustavoSept.Cron@gmail.com>
2024-10-03 08:36:07 -0300

    moves pipe and interceptor from main.ts into AppModule

--- ez_decks/src/app.module.ts
+++ ez_decks/src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { PrismaService } from './prisma/prisma.service';
 import { WordEngineModule } from './word_engine/word_engine.module';
-import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './prisma/prisma.service';
+import { DatabaseInterceptor } from './common/errors/interceptors/database.interceptor';
 import { DictGeneratorModule } from './dict-generator/dict-generator.module';
 import { node_env } from './common/config/constants';
 
@@ -20,6 +22,21 @@ import { node_env } from './common/config/constants';
       }),
    ],
    controllers: [AppController],
-   providers: [AppService, PrismaService],
+   providers: [
+      AppService,
+      PrismaService,
+      {
+         provide: APP_PIPE,
+         useValue: new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+            transform: true,
+         }),
+      },
+      {
+         provide: APP_INTERCEPTOR,
+         useClass: DatabaseInterceptor,
+      },
+   ],
 })
 export class AppModule {}
--- ez_decks/src/main.ts
+++ ez_decks/src/main.ts
@@ -1,9 +1,7 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import { node_env } from './common/config/constants';
-import { DatabaseInterceptor } from './common/errors/interceptors/database.interceptor';
 
 async function bootstrap() {
    console.log(`\n\nRunning on ${node_env} environment!\n\n`);
@@ -15,16 +13,6 @@ async function bootstrap() {
 
    // Retrieve the port from ConfigService (with a fallback to 4000)
    const appPort = configService.get<string>('NODEAPP_PORT', '4000');
-
-   app.useGlobalPipes(
-      new ValidationPipe({
-         whitelist: true,
-         forbidNonWhitelisted: true,
-         transform: true,
-      })
-   );
-
-   app.useGlobalInterceptors(new DatabaseInterceptor());
 
    await app.listen(appPort);
 }
