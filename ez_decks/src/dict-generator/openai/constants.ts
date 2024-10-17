b2b10b5041db0d882a6f6ef1b37a70165a62bc17
GustavoSept <GustavoSept.Cron@gmail.com>
2024-10-16 21:16:20 -0300

    lowers DEFAULT_MAX_TOKEN_OUTPUT from 2k to 1152

--- ez_decks/src/dict-generator/openai/constants.ts
+++ ez_decks/src/dict-generator/openai/constants.ts
@@ -2,7 +2,7 @@ export const OPENAI_SDK = 'OPENAI_SDK';
 
 export const OPENAI_DEFAULT_FALLBACK_MODEL = 'gpt-4o-mini';
 
-export const DEFAULT_MAX_TOKEN_OUTPUT = 2048;
+export const DEFAULT_MAX_TOKEN_OUTPUT = 1152; // 1024 + 128
 
 export const DEFAULT_USER_MESSAGE_PREFIX = 'Translate the following words: ';
 
